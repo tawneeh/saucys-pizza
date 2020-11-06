4327e93b3e7c34c800787f879b38a974e77cc817
Tawnee Harris <tawneeh@icloud.com>
2020-11-06 10:01:25 -0800

    addPizza method successful

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
+//Business Logic for Pizza Orders -----
+function PizzaOrder() {
+  this.pizzas = [];
+}
+
+PizzaOrder.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+
+// Business Logic for Contacts ---------
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
+}
+
+
+Pizza.prototype.calculatePrice = function() {
+
 }
