ee45fcd33706e57a2f1bc1608859d27138e7becd
Tawnee Harris <tawneeh@icloud.com>
2020-11-06 09:45:53 -0800

    attempting test 1

--- _tests_/scripts.test.js
+++ _tests_/scripts.test.js
@@ -1,15 +1,13 @@
 // Tests
 
-// Describe: functionName
+// Describe: addPizza()
+
+// Test: should add pizza to the user order
+// Code: pizzaOrder.addPizza(small cheese pizza)
+// Expect: order = [small cheese pizza]
 
 // Test: 
-// Code:  
 // Expect:
 
-// Test: 
-// Code:  
-// Expect:
-
-// Test: 
-// Code:  
+// Test:  
 // Expect:
